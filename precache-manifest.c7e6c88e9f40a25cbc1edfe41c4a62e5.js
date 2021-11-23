f1e67febea8e8f5d6073a22d7c4eed575162c19d
Pietro Manneschi <30522474+pietromanneschi@users.noreply.github.com>
2021-11-23 12:05:24 +0100

    Add files via upload

---
+++ precache-manifest.c7e6c88e9f40a25cbc1edfe41c4a62e5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5277a7dda06f5e9e9a9",
+    "url": "/css/app.a853f9f8.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "fa504458c4776c2b3f1243952ee17779",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ebfb1b21ce4b09f4068",
+    "url": "/js/about.d41a4570.js"
+  },
+  {
+    "revision": "d5277a7dda06f5e9e9a9",
+    "url": "/js/app.23c00c32.js"
+  },
+  {
+    "revision": "e306a9b5a2d6fe3e2457",
+    "url": "/js/chunk-vendors.4b6a23f5.js"
+  },
+  {
+    "revision": "a5932f6d5406b5ce4ca02c00f587610f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
